699c2a2a3d7394537bf70980cc7ffa2ddadd368a
NicoleQR <nquiros20855@ufide.ac.cr>
2025-12-10 12:08:58 -0600

    Actividad

--- src/main/java/com/grupo3/ProyectoLBD/repository/ActividadGrupoViewRepository.java
+++ src/main/java/com/grupo3/ProyectoLBD/repository/ActividadGrupoViewRepository.java
@@ -1,13 +1,16 @@
 package com.grupo3.ProyectoLBD.repository;
 
+import com.grupo3.ProyectoLBD.model.ActividadGrupo;
 import com.grupo3.ProyectoLBD.model.ActividadGrupoView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 @Repository
-public interface ActividadGrupoViewRepository extends JpaRepository<ActividadGrupoView, Integer> {
+public interface ActividadGrupoViewRepository 
+       extends JpaRepository<ActividadGrupoView, ActividadGrupo>{
 
     // Buscar por ID de actividad
     List<ActividadGrupoView> findByIdActividad(Integer idActividad);
